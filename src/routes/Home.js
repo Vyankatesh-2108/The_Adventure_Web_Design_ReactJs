3c181d1d25f185f140bee3f112f5b7ea0a146d56
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-10-20 11:50:22 +0530

    The Adventure Web Desing Using React Js

---
+++ src/routes/Home.js
@@ -0,0 +1,26 @@
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar"
+import HomeImgage from "../assets/home.jpg"
+import Details from "../components/Details";
+import Footer from "../components/Footer"
+
+function Home () {
+    return(
+        <>
+        <Navbar/>
+            <Hero 
+                cName="hero"
+                heroImg={HomeImgage}
+                title="Your Journey Your Story"
+                text="Choose Your Favorite Destination."
+                buttonText="Travel Plan"
+                url="/"
+                btnClass="show"
+            />
+        <Details/>
+        <Footer />
+        </>
+    )
+}
+
+export default Home;
